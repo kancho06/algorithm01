331094e00339211aff3019d5a56c075ce0275931
kancho06 <wotjd5792@gmail.com>
2022-02-17 22:25:53 +0900

    2/17 백준 알고리즘 그리디 알고리즘 공부
    2/17 백준 알고리즘 그리디 알고리즘 공부

---
+++ baekjoon_greedy_1~50/1339_word_mathematics.py
@@ -0,0 +1,45 @@
+# 문제
+# 민식이는 수학학원에서 단어 수학 문제를 푸는 숙제를 받았다.
+#
+# 단어 수학 문제는 N개의 단어로 이루어져 있으며, 각 단어는 알파벳 대문자로만 이루어져 있다.
+# 이때, 각 알파벳 대문자를 0부터 9까지의 숫자 중 하나로 바꿔서 N개의 수를 합하는 문제이다.
+# 같은 알파벳은 같은 숫자로 바꿔야 하며, 두 개 이상의 알파벳이 같은 숫자로 바뀌어지면 안 된다.
+#
+# 예를 들어, GCF + ACDEB를 계산한다고 할 때, A = 9, B = 4, C = 8, D = 6, E = 5, F = 3, G = 7로 결정한다면, 두 수의 합은 99437이 되어서 최대가 될 것이다.
+#
+# N개의 단어가 주어졌을 때, 그 수의 합을 최대로 만드는 프로그램을 작성하시오.
+#
+# 입력
+# 첫째 줄에 단어의 개수 N(1 ≤ N ≤ 10)이 주어진다.
+# 둘째 줄부터 N개의 줄에 단어가 한 줄에 하나씩 주어진다.
+# 단어는 알파벳 대문자로만 이루어져있다.
+# 모든 단어에 포함되어 있는 알파벳은 최대 10개이고, 수의 최대 길이는 8이다.
+# 서로 다른 문자는 서로 다른 숫자를 나타낸다.
+#
+# 출력
+# 첫째 줄에 주어진 단어의 합의 최댓값을 출력한다.
+
+n = int(input())
+alphabet_dict = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0,
+                 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
+word_list = []
+sum_list = []
+result = 0
+for i in range(n):
+    word = input()
+    word_list.append(word)
+
+for word in word_list:
+    for i in range(len(word)):
+        num = 10 ** (len(word) - i - 1)  # 단어의 자리수 파악 (i=0 부터 시작하기때문에 -1 을 해준다) (ex. abc = 10 ** (3 - 0 - 1) = 100의 자리)
+        alphabet_dict[word[i]] += num
+
+for value in alphabet_dict.values():
+    if value > 0:
+        sum_list.append(value)
+
+sorted_list = sorted(sum_list, reverse=True)
+for i in range(len(sorted_list)):
+    result += sorted_list[i] * (9 - i)
+
+print(result)
